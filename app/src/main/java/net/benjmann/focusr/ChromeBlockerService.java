5aa43cf0ee5c5d172a44af4036541b13c0c5d73e
Benjamin Mann <8enmann@gmail.com>
2017-02-03 21:04:31 -0800

    Initial commit

---
+++ app/src/main/java/net/benjmann/focusr/ChromeBlockerService.java
@@ -0,0 +1,202 @@
+package net.benjmann.focusr;
+
+import android.accessibilityservice.AccessibilityService;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Toast;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ChromeBlockerService extends AccessibilityService {
+
+    // TODO: serialize this to save across phone restarts.
+    private static long timeMillis;
+    private static long lastStart;
+    // Used to track 24 hours. Make this configurable.
+    private static long firstStart;
+    private static boolean isRunning;
+    private static Set<String> apps;
+    private static long appsLastUpdated;
+    // Lazy load settings updates every 30 seconds.
+    private static final int REFRESH_INTERVAL_MILLIS = 30 * 1000;
+    private static HashMap<String, Long> lastPreferenceCheck = new HashMap<>();
+    private static HashMap<String, Float> lastPreferenceValue = new HashMap<>();
+
+    private float getPreferenceLazy(String key, float def) {
+        Long lastCheck = lastPreferenceCheck.get(key);
+        if (lastCheck == null) {
+            lastCheck = 0L;
+        }
+        float value = def;
+        if (System.currentTimeMillis() - lastCheck > REFRESH_INTERVAL_MILLIS) {
+            lastPreferenceCheck.put(key, System.currentTimeMillis());
+
+            try {
+                value = Float.parseFloat(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(key, Float.toString(def)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            lastPreferenceValue.put(key, value);
+        } else {
+            return lastPreferenceValue.get(key);
+        }
+        return value;
+    }
+
+    long getTimeSpentMillis() {
+        return timeMillis;
+    }
+
+    @Override
+    public void onInterrupt() {
+        Log.i(getPackageName(), "Interrupt");
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.i(getPackageName(), "Connected");
+    }
+
+    private static String eventToString(AccessibilityEvent event) {
+        CharSequence[] fields = {
+                event.getPackageName(),
+                event.getContentDescription(),
+                event.getClassName(),
+                AccessibilityEvent.eventTypeToString(event.getEventType()),
+                Integer.valueOf(event.getEventType()).toString(),
+        };
+        return TextUtils.join(" ", fields);
+    }
+
+
+    synchronized private Set<String> getPackagesToTrack() {
+        if (System.currentTimeMillis() - appsLastUpdated > REFRESH_INTERVAL_MILLIS) {
+            appsLastUpdated = System.currentTimeMillis();
+            apps = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getStringSet("apps", new HashSet<String>());
+        }
+        return apps;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        final int eventType = event.getEventType();
+        String eventText = null;
+        if (event.getPackageName().toString().isEmpty()) {
+            Log.i(getPackageName(), event.getClassName() + " " + AccessibilityEvent.eventTypeToString(event.getEventType()));
+            // Ignore these events since they don't seem to trigger on anything important.
+            return;
+        }
+
+        if (getPackagesToTrack().contains(event.getPackageName().toString())) {
+            startTimer(event.getEventTime());
+            return;
+        }
+        if (!event.getPackageName().equals("com.android.chrome")) {
+            stopTimer(event.getEventTime(), event.getPackageName().toString());
+        }
+
+        AccessibilityNodeInfo source = event.getSource();
+        if (source == null) {
+            return;
+        }
+        // TODO: support other browsers?
+        List<AccessibilityNodeInfo> nodes = source.findAccessibilityNodeInfosByViewId("com.android.chrome:id/url_bar");
+        for (AccessibilityNodeInfo node : nodes) {
+            String original = node.getText().toString();
+            String domain = getDomainName(original);
+            Log.i(getPackageName(), domain);
+            if (domain.endsWith("facebook.com")) {
+                startTimer(event.getEventTime());
+            } else if (domain.trim().isEmpty()) {
+                Log.i(getPackageName(), "Skipping " + original);
+            } else {
+                stopTimer(event.getEventTime(), domain);
+            }
+            node.recycle();
+        }
+    }
+
+    private void startTimer(long eventTime) {
+        Log.i(getPackageName(), "start " + isRunning);
+        // Reset every 24 hours
+        if ((eventTime - firstStart) > 24 * 60 * 60 * 1000) {
+            Log.i(getPackageName(), "init");
+            firstStart = eventTime;
+            timeMillis = 0;
+        }
+        if (isRunning) {
+            // If it's been more than a minute since the last start, toast anyway
+            if (eventTime - lastStart > 1000 * 60 * getPreferenceLazy("progress_interval", 1)) {
+                timeMillis += eventTime - lastStart;
+                lastStart = eventTime;
+                showTime(timeMillis);
+            }
+            return;
+        }
+        isRunning = true;
+        lastStart = eventTime;
+        showTime(timeMillis);
+
+    }
+
+    public static String getDomainName(String url)  {
+        // URI barfs if there's no scheme.
+        if (!url.startsWith("http")) {
+            url = "http://" + url;
+        }
+        URI uri = null;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return "";
+        }
+        String domain = uri.getHost();
+        if (domain == null) {
+            Log.w(ChromeBlockerService.class.getPackage().toString(), url);
+            return "";
+        }
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    private void stopTimer(long eventTime, String reason) {
+        if (reason.trim().isEmpty()) {
+            reason = "empty";
+        }
+        Log.i(getPackageName(), "stop " + isRunning + " " + reason);
+        if (!isRunning) {
+            return;
+        }
+        isRunning = false;
+        timeMillis += eventTime - lastStart;
+        showTime(timeMillis);
+    }
+
+    private void showTime(long millis) {
+        Log.i(getPackageName(), millis + " " + isRunning + " " + lastStart);
+        double mins = getPreferenceLazy("total_time_per_day", 10) - (millis / 1000.0 / 60);
+
+        String message = String.format("%.1f mins remaining", mins);
+        if (mins < 0) {
+            message = "Time's up!";
+            // TODO: consider kicking them back to home screen.
+            // this.performGlobalAction(GLOBAL_ACTION_HOME);
+        }
+        Toast.makeText(
+                this.getApplicationContext(),
+                message,
+                Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/net/benjmann/focusr/SettingsActivity.java
@@ -0,0 +1,84 @@
+package net.benjmann.focusr;
+
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBar;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * A {@link PreferenceActivity} that presents a set of application settings. On
+ * handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ * <p>
+ * See <a href="http://developer.android.com/design/patterns/settings.html">
+ * Android Design: Settings</a> for design guidelines and the <a
+ * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
+ * API Guide</a> for more information on developing a Settings UI.
+ */
+public class SettingsActivity extends AppCompatPreferenceActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+        SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
+        onSharedPreferenceChanged(sp, "total_time_per_day");
+        onSharedPreferenceChanged(sp, "apps");
+        onSharedPreferenceChanged(sp, "progress_interval");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference pref = findPreference(key);
+        if (pref instanceof EditTextPreference) {
+            EditTextPreference etp = (EditTextPreference) pref;
+            pref.setSummary(etp.getText());
+        } else if (pref instanceof MultiSelectListPreference){
+            MultiSelectListPreference mslp = (MultiSelectListPreference) pref;
+            ArrayList<CharSequence> items = new ArrayList<>();
+            CharSequence[] entries = mslp.getEntries();
+            for (String value : sharedPreferences.getStringSet(key, new HashSet<String>())) {
+                items.add(entries[mslp.findIndexOfValue(value)]);
+            }
+
+            pref.setSummary(TextUtils.join(", ", items));
+        }
+    }
+}
